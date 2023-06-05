0c83625c7133027002b4778962ba6824faa9e02e
WELLYTON DOS SANTOS PEDROSO <wellyton.0957@aluno.pr.senac.br>
2023-06-05 20:52:12 -0300

    main

---
+++ Struct_01.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+//Biblioteca para conseguir utilizar fgets
+#include<string.h>
+
+//Struct
+typedef struct{
+	int codigo;
+	char nome[15];
+	int quantidade;
+} Estoque;
+
+
+int main(){
+	//Declarando produtos
+	Estoque produto1;
+	Estoque produto2;
+	//Armazenando codigo do produto via scan
+	scanf ("%d%*c", &produto1.codigo);
+	//Armazenando codigo forma padrão
+	produto2.codigo = 2;
+	
+	//Armazenando string utilizando fgets
+	//stdin que dizer que estou recebendo valor do teclado, mas poderia ser um arquivo
+	fgets(produto1.nome,15,stdin);
+	//Limpando buffer
+	fflush(stdin);
+	//Armazenando valor padrão 
+	strcpy(produto2.nome,"Raquete");
+	
+	//Armazenando valor de quantidade
+	scanf("%d%*c", &produto1.quantidade);	
+	produto2.quantidade = 104;
+	
+	//Imprimindo produto01
+	printf("Codigo: %d \n", produto1.codigo);
+	printf("Nome: %s", produto1.nome);
+	printf("Quantidade: %d \n", produto1.quantidade);	
+	
+	//Imprimindo produto02
+	printf("Codigo: %d \n", produto2.codigo);
+	printf("Nome: %s \n", produto2.nome);
+	printf("Quantidade: %d \n", produto2.quantidade);
+	return 0;
+	
+}
---
+++ Struct_02.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+//Struct
+typedef struct{
+	int cadastro;
+	float imposto;
+	int atrasos;
+	float multa;
+	float total;
+}Imovel;
+
+int main(){
+	//Declarando 
+	Imovel imovel01;
+	
+	//Armazenando valores 
+	printf("Cadastro do imovel: ");
+	scanf("%d", &imovel01.cadastro);
+	printf("Valor imposto imovel: ");
+	scanf("%f", &imovel01.imposto);
+	printf("Meses atraso: ");
+	scanf("%d", &imovel01.atrasos);
+	
+	// Se o valor de meses em atraso for maior que 0 entra no if
+	if(imovel01.atrasos > 0 ){
+		
+		imovel01.total = imovel01.imposto + (imovel01.atrasos * 50);
+		imovel01.multa = imovel01.atrasos * 50;
+		
+	}
+	
+	//Mostrando na tela o resultado	
+	printf("Cadastro imovel: %d \n", imovel01.cadastro);
+	printf("Imposto do imovel: %.2f \n", imovel01.imposto);
+	printf("Meses em atraso: %d \n", imovel01.atrasos);
+	printf("Valor total de multa: %.2f \n", imovel01.multa);
+	printf("Valot total a pagar: %.2f \n", imovel01.total);
+	
+	return 0;
+}
